4e6445a2aaaf5076b519f38744f9339039895077
Felipe Gil <fgil90@gmail.com>
2021-04-02 23:15:39 -0300

    Funcionality done. Missing some mobile stuff.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 
+//variables
+
+let main = findElement("main");
+let filtersContainer = findElement(".filters-container");
+let filtersLeft = findElement(".filters-left");
+
+let currentFilters = [];
+
+//function definitions
 
 function findElement(id) {
     return document.querySelector(id);
@@ -8,139 +17,118 @@ function findElements(id) {
     return document.querySelectorAll(id);
 }
 
+function removeThis(event) {
+    event.target.parentNode.parentNode.removeChild(event.target.parentNode);
+}
 
+function changeFilters(event) {
 
-let main = findElement("main");
-//createComponents();
+    let f = event.target.textContent;
 
-function createComponents() {
-    let component = '';
+    //checks if filters include f. If it does, remove it, if it doesn't, add it.
+    (currentFilters.includes(f)) ? currentFilters.splice(currentFilters.indexOf(f), 1) : currentFilters.push(f);
 
+    populateFiltersContainer();
+    filterResults();
+
+}
+
+function populateFiltersContainer() {
+
+    filtersLeft.innerHTML = '';
+
+    currentFilters.forEach(element => {
+        filtersLeft.innerHTML +=
+            `<div class="filter-container">
+            <p onclick="changeFilters(event)" class="filter">${element}</p>
+            <p class="x-button">&MediumSpace; &#9587;</p>
+        </div>`;
+    });
+
+    (filtersLeft.innerHTML == '') ? filtersContainer.classList.add('hidden') : filtersContainer.classList.remove('hidden');
+
+}
+
+function removeAllFilters() {
+    currentFilters = [];
+    populateFiltersContainer();
+    filterResults();
+}
+
+function populateMainDiv() {
+    main.innerHTML = '';
 
     data.forEach(element => {
         let lang = '';
         let tools = '';
         if (element.languages.length > 0) {
             for (i = 0; i < element.languages.length; i++) {
-                lang += "<p class='language'>" + element.languages[i] + "</p>";
+                lang += "<button value='languages' onclick='changeFilters(event)' class='language'>" + element.languages[i] + "</button>";
             }
         }
 
         if (element.tools.length > 0) {
             for (i = 0; i < element.tools.length; i++) {
-                tools += "<p class='tool'>" + element.tools[i] + "</p>";
+                tools += "<button value='tools' onclick='changeFilters(event)'  class='tool' >" + element.tools[i] + "</button>";
             }
         }
 
-
-        component +=
+        let component =
             `<div class="component">
-    <div class="logo-container">
-      <img class="logo" src="${element.logo}" alt="">
-    </div>
-    <section class="info-left">
-      <em class="company">${element.company}</em>
-      ${element.new ? "<h2 class='new'>NEW!</h2>" : ''}
-      ${element.featured ? "<h2 class='featured'>FEATURED!</h2>" : ''}
+                <div class="logo-container">
+                    <img class="logo" src="${element.logo}" alt="">
+                </div>
+                <section class="info-left">
+                    <em class="company">${element.company}</em>
+                    ${element.new ? "<h2 class='new'>NEW!</h2>" : ''}
+                    ${element.featured ? "<h2 class='featured'>FEATURED!</h2>" : ''}
 
-      <h1 class="position">${element.position}</h1>
-      <ul>
-        <li class="postedAt">${element.postedAt}</li>
-        <li class="contract">${element.contract}</li>
-        <li class="location">${element.location}</li>
-      </ul>
-      
-    </section>
-    <section class="info-right">
-    <p class="role">${element.role}</p>
-    <p class="level">${element.level}</p>
-    ${lang}
-    ${tools}
-    </section>
-</div>`
+                    <h1 class="position">${element.position}</h1>
+                    <ul>
+                        <li class="postedAt">${element.postedAt}</li>
+                        <li class="contract">${element.contract}</li>
+                        <li class="location">${element.location}</li>
+                    </ul>
+                    
+                </section>
+                <section class="info-right">
+                    <button value="role" class="role" onclick="changeFilters(event)">${element.role}</button>
+                    <button value="level" class="level" onclick="changeFilters(event)">${element.level}</button>
+                    ${lang}
+                    ${tools}
+                </section>
+            </div>`
 
+        main.innerHTML += component;
+    });
+}
+
+function filterResults() {
+    let componentArray = findElements(".component");
+    componentArray.forEach(element => {
+        element.classList.remove('hidden');
     });
 
-    main.innerHTML = component;
+    componentArray.forEach(elementA => {
+        currentFilters.forEach(elementF => {
+            
+            let regex = new RegExp(elementF, "i");
+
+            if (!regex.test(elementA.innerHTML)) {
+                elementA.classList.add('hidden');
+            } else if(!elementA.classList.contains('hidden')){
+                elementA.classList.remove('hidden');
+            }
+        });
+    });
 }
 
 
 
-function searchInData(data, property, query) {
-    let size = data.length;
-    let propertyArray = [];
-    let regexp = new RegExp(query, "i")
+//function calls
 
-    for (i = 0; i < size; i++) {
+populateMainDiv();
 
-        let value = data[i][property];
-        let isQueryInValue = regexp.test(value.toString());
+a = findElement('.component');
 
-        if (isQueryInValue) {
-            propertyArray.push(data[i]["id"]);
-        }
-
-    }
-
-    return propertyArray;
-
-}
-
-//Create main component div
-
-
-
-    // let logoContainer = addElement('div', "logo-container", component);
-    // let logo = addElement('img', "logo", logoContainer);
-    // let infoLeft = addElement('section', "info-left", component);
-    // let company = addElement('em', "company", infoLeft);
-    // let _new = addElement('h2', "new", infoLeft);
-    // let featured = addElement('h2', "featured", infoLeft);
-    // let position = addElement('h1', "position", infoLeft);
-    // let ul = addElement('ul', "ul-left", infoLeft);
-    // let postedAt = addElement('li', "postedAt", ul);
-    // let contract = addElement('li', "contract", ul);
-    // let location = addElement('li', "location", ul);
-    // let infoRight = addElement('section', "info-right", component);
-    // let role = addElement('p', "role", infoRight);
-    // let level = addElement('p', "section", infoRight);
-
-    // let langArray = element.languages;
-    // let toolsArray = element.tools;
-
-    // logo.src = element.logo;
-    // company.textContent = element.company;
-    // _new.textContent = "NEW!"
-    // featured.textContent = "FEATURED"
-
-    // if (!element.new){
-    //     _new.classList.add("hidden");
-    // }
-    // if (!element.featured){
-    //     featured.classList.add("hidden");
-    // }
-
-    // position.textContent = element.position;
-    // postedAt.textContent = element.postedAt;
-    // contract.textContent = element.contract;
-    // location.textContent = element.location;
-    // role.textContent = element.role;
-    // level.textContent = element.level;
-
-    // langArray.forEach(lang =>{
-    //     l = addElement('p', "language", infoRight);
-    //     l.textContent = lang;
-    // })
-
-    // toolsArray.forEach(tool =>{
-    //     t = addElement('p', "tool", infoRight);
-    //     t.textContent = tool;
-    // })
-
-    // function addElement(tag, cssClass, attachTo) {
-//     let obj = document.createElement(tag);
-//     obj.classList.add(cssClass)
-//     attachTo.appendChild(obj);
-
-//     return obj;
-// }
+console.log(a.textContent);
